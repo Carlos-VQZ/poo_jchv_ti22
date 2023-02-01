cabd987c583071422f1c97f58b1314591a021675
CarlosVQZ <122801355+Carlos-VQZ@users.noreply.github.com>
2023-02-01 14:53:33 +0000

    SE CREO programa7.py en el cual se compararon 2 números enteros y se imprimió el mayor mediante el uso de la condición if else -Recuerda la estructura de los operadores de comparación Se realizaron los cambios en el documento  README.md

---
+++ semana3/programa7.py
@@ -0,0 +1,15 @@
+""" Programa7
+    Nombre: Juan Carlos Hernandez Vazquez
+    Fecha: 01/02/2023
+    Descripcion: Comparar 2 numeros enteros en imprimir el numero mayor"""
+
+numero1 = int(input("Ingresa un numero entero: "))  #  funcion imput para introducir y casteo para introducir numeros enteros
+numero2 = int(input("Ingresa otro numero entero: "))  #  funcion imput para introducir y casteo para introducir datos 
+
+if numero1>=numero2: #  Funcion if para realizar una condición si es verdadrea se ejecuta el codigo
+    print(numero1)  #  Secuencia de ordenes si la condición if se cumple
+else:   #  funcion else para realizar una 2da condicion si la condición de la funcion if es falsa
+    print(numero2)  #  secuencia de ordenes si la condición if no se cumple
+
+
+    
